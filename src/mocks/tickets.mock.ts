6fc83f4c3cbdd30593b3c28792cc998f3b846dd5
Tacherifte Aymen <ch.tacherifte@gmail.com>
2023-02-03 18:15:32 +0100

    0 - 1 : modify tickets title

--- src/mocks/tickets.mock.ts
+++ src/mocks/tickets.mock.ts
@@ -4,13 +4,13 @@ const dateToday: Date = new Date();
 
 export const TICKETS_MOCKED: Ticket[] = [
   {
-    title: 'SI4 in Madrid',
+    title: 'SI4 in Argentina',
     description: '',
     date: dateToday,
     author: 'Paul'
   },
   {
-    title: 'SI5 in Paris',
+    title: 'SI5 in Russia',
     description: 'Description du voyage',
     date: dateToday,
     author: 'Anakin'

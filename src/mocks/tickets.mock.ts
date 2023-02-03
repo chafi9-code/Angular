09c2ed1d29cd956696ab93b5b076c6aeeb8b099d
Tacherifte Aymen <ch.tacherifte@gmail.com>
2023-02-03 19:06:36 +0100

    0 - 5 : fix bug of compilation

--- src/mocks/tickets.mock.ts
+++ src/mocks/tickets.mock.ts
@@ -7,12 +7,12 @@ export const TICKETS_MOCKED: Ticket[] = [
     title: 'SI4 in Argentina',
     description: '',
     date: dateToday,
-    author: 'Paul'
+    student: 'Paul'
   },
   {
     title: 'SI5 in Russia',
     description: 'Description du voyage',
     date: dateToday,
-    author: 'Anakin'
+    student: 'Anakin'
   },
 ];
